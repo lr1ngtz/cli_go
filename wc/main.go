131b5a35cd52fe25c185874ff9bf6977bd2d6274
lr1ngtz <sko0hlol@gmail.com>
2024-11-26 23:18:53 +0100

    Word counter CLI app

---
+++ wc/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"io"
+	"os"
+)
+
+func count(r io.Reader, countlines bool) int {
+	scanner := bufio.NewScanner(r)
+	if !countlines {
+		scanner.Split(bufio.ScanWords)
+	}
+	wc := 0
+	for scanner.Scan() {
+		wc++
+	}
+
+	return wc
+}
+
+func main() {
+	lines := flag.Bool("l", false, "Count lines")
+	flag.Parse()
+	fmt.Println(count(os.Stdin, *lines))
+}
