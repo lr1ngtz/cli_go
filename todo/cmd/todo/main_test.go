d0b64f40fd8fdd5be186fe7618f9a47755529195
lr1ngtz <sko0hlol@gmail.com>
2024-12-01 19:52:14 +0100

    Exercises: ToDo

--- todo/cmd/todo/main_test.go
+++ todo/cmd/todo/main_test.go
@@ -12,7 +12,7 @@ import (
 
 var (
 	binName  = "todo"
-	fileName = ".todo.json"
+	fileName = os.Getenv("TODO_FILENAME")
 )
 
 func TestMain(m *testing.M) {
@@ -37,7 +37,7 @@ func TestMain(m *testing.M) {
 }
 
 func TestTodoCLI(t *testing.T) {
-	task := "test task number 1"
+	task1 := "test task number 1"
 
 	dir, err := os.Getwd()
 	if err != nil {
@@ -47,7 +47,7 @@ func TestTodoCLI(t *testing.T) {
 	cmdPath := filepath.Join(dir, binName)
 
 	t.Run("AddNewTaskFromArguments", func(t *testing.T) {
-		cmd := exec.Command(cmdPath, "-add", task)
+		cmd := exec.Command(cmdPath, "-add", task1)
 		if err := cmd.Run(); err != nil {
 			t.Fatal(err)
 		}
@@ -68,6 +68,23 @@ func TestTodoCLI(t *testing.T) {
 		}
 	})
 
+	task3 := "test task number 3"
+	task4 := "test task number 4"
+	t.Run("AddMultipleTasksFromStdin", func(t *testing.T) {
+		cmd := exec.Command(cmdPath, "-add")
+		cmdStdin, err := cmd.StdinPipe()
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		io.WriteString(cmdStdin, task3+"\n"+task4)
+		cmdStdin.Close()
+
+		if err := cmd.Run(); err != nil {
+			t.Fatalf("Error running the command: %v", err)
+		}
+	})
+
 	t.Run("ListTasks", func(t *testing.T) {
 		cmd := exec.Command(cmdPath, "-list")
 		out, err := cmd.CombinedOutput()
@@ -75,7 +92,7 @@ func TestTodoCLI(t *testing.T) {
 			t.Fatal(err)
 		}
 
-		expected := fmt.Sprintf("  1: %s\n  2: %s\n", task, task2)
+		expected := fmt.Sprintf("  1: %s\n  2: %s\n  3: %s\n  4: %s\n", task1, task2, task3, task4)
 		if expected != string(out) {
 			t.Errorf("Expected %q, got %q instead\n", expected, string(out))
 		}
